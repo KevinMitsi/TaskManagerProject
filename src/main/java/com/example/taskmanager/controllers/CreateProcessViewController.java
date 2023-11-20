b076b91aa3e76043a12668da8ccbb80b3067ee6d
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2023-11-19 23:43:08 -0500

    +++--

--- src/main/java/com/example/taskmanager/controllers/CreateProcessViewController.java
+++ src/main/java/com/example/taskmanager/controllers/CreateProcessViewController.java
@@ -1,16 +1,16 @@
 package com.example.taskmanager.controllers;
 
 import com.example.taskmanager.TaskApp;
+import com.example.taskmanager.exceptions.ProcessException;
 import com.example.taskmanager.model.person.Admin;
 import com.example.taskmanager.model.person.Common;
-import com.example.taskmanager.model.process.Activity;
-import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class CreateProcessViewController {
+    public TextField tfDescrpcion;
     TaskApp main;
-    ModelFactoryController singleton = ModelFactoryController.getInstance();
     Admin admin;
     Common selectedCommon;
     public void setMain(TaskApp taskApp, Admin admin, Common common) {
@@ -20,7 +20,22 @@ public class CreateProcessViewController {
        }
 
 
-    public void onVolverClick(ActionEvent event) throws IOException {
+    public void onVolverClick() throws IOException {
         main.abrirPanelAdmin(admin);
+    }
+
+    public void onCrearClick() {
+        if (!tfDescrpcion.getText().isBlank()){
+            try{
+                admin.createProcess("P"+selectedCommon.getProcesses().size()+1,tfDescrpcion.getText(),selectedCommon);
+                Alerta.saltarAlertaInformacion("El proceso se agregó correctamente");
+                tfDescrpcion.setText("");
+            } catch (ProcessException e) {
+                Alerta.saltarAlertaError(e.getMessage());
+            }
+        }
+        else {
+            Alerta.saltarAlertaError("No puede dejar el nombre vacío");
+        }
     }
 }
